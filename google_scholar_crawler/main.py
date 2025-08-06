19d7fa6f513ffcaaf02f61e9a4c16db7158ae609
Shicong Liu <scliubit@gmail.com>
2025-08-06 11:26:31 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,35 +1,43 @@
 from scholarly import scholarly, ProxyGenerator
 import logging
-from fp.fp import FreeProxy
+import time
+# from fp.fp import FreeProxy
 # import jsonpickle
 import json
 from datetime import datetime
 import os
 logging.basicConfig(level=logging.INFO)
 
-try:
-    logging.info("Script started")
-    proxy = FreeProxy().get()
-    logging.info(f"Proxy: {proxy}")
-    pg = ProxyGenerator()
-    pg.FreeProxies()
-    scholarly.use_proxy(pg)
+MAX_RETRIES = 10
+RETRY_DELAY = 60  # seconds
+GOOGLE_SCHOLAR_ID = 'x47f3O4AAAAJ'
 
-    GOOGLE_SCHOLAR_ID = 'x47f3O4AAAAJ'
-    author: dict = scholarly.search_author_id(GOOGLE_SCHOLAR_ID)
-    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-    name = author['name']
-    author['updated'] = str(datetime.now())
-    author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-except Exception as e:
-    logging.error(f"Error occurred: {e}")
-    raise
+
+for _ in range(MAX_RETRIES):
+    try:
+        logging.info("Script started")
+        # proxy = FreeProxy().get()
+        # logging.info(f"Proxy: {proxy}")
+        pg = ProxyGenerator()
+        pg.FreeProxies()
+        scholarly.use_proxy(pg)
+        author: dict = scholarly.search_author_id(GOOGLE_SCHOLAR_ID)
+        logging.info(f"Success with {_} attempts")
+        break
+    except Exception as e:
+        logging.warning(f"Failed to set sleep time, retrying... {e}")
+        time.sleep(RETRY_DELAY)
+
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
+author['updated'] = str(datetime.now())
+author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
-print('Create results/gs_data.json Done')
+logging.info('Create results/gs_data.json Done')
 
 
 shieldio_data = {
@@ -40,4 +48,4 @@ shieldio_data = {
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
 
-print('Create results/gs_data_shieldsio.json Done')
+logging.info('Create results/gs_data_shieldsio.json Done')
